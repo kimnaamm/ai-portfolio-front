8f73ca314a4ecbf2f9f96d4c915f94a85aa0d5c8
unknown <topia.naam@gmail.com>
2025-08-08 14:28:24 +0900

    fix vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+// vitest.config.ts
+import { defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
@@ -7,8 +7,6 @@ export default mergeConfig(
   defineConfig({
     test: {
       environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
     },
-  }),
+  })
 )
